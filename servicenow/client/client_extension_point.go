774cc4f57a0f9731f7d99c09943e89da7fb0c684
Michael Grondines <mic.grondines@gmail.com>
2019-07-31 15:49:59 -0400

    Slight modif

--- servicenow/client/client_extension_point.go
+++ servicenow/client/client_extension_point.go
@@ -1,9 +1,9 @@
 package client
 
-// EndpointExtensionPoint is the endpoint to manage JS Include records.
-const EndpointExtensionPoint = "sp_js_include.do"
+// EndpointExtensionPoint is the endpoint to manage Extension Point records.
+const EndpointExtensionPoint = "sys_extension_point.do"
 
-// ExtensionPoint represents the json response for a Js Include in ServiceNow.
+// ExtensionPoint represents the json response for a Extension Point in ServiceNow.
 type ExtensionPoint struct {
 	BaseResult
 	Name          string `json:"name"`
